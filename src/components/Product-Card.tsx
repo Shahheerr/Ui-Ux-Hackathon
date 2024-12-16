73cafbace466b8ddc5d042cd2a4135524a7a5410
Shahheerr <159336048+Shahheerr@users.noreply.github.com>
2024-12-16 19:07:49 +0500

    Update Product-Card.tsx

--- src/components/Product-Card.tsx
+++ src/components/Product-Card.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { ShoppingCart } from 'lucide-react'
 
 interface ProductCardProps {
-  imageSrc: string | unknown
+  imageSrc: string
   name: string
   price: number
   originalPrice?: number
